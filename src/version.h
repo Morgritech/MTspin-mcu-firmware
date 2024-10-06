5ec994be8bcbfc2b294c19bf387368cec4bc3c70
JM <jo3tech@outlook.com>
2024-10-06 21:29:07 +0100

    Updated version.

--- src/version.h
+++ src/version.h
@@ -14,10 +14,10 @@
 namespace mtspin {
 
 inline constexpr char* kName = "mtspin-mcu-firmware-";
-inline constexpr uint16_t kMajor = 0;
-inline constexpr uint16_t kMinor = 1;
+inline constexpr uint16_t kMajor = 1;
+inline constexpr uint16_t kMinor = 0;
 inline constexpr uint16_t kPatch = 0;
-inline constexpr char* kSuffix = " ";
+inline constexpr char* kSuffix = "-rc.1";
 
 } // namespace mtspin
 
